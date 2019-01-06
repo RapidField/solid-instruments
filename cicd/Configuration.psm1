b26bc919eef813da7b285055615b6bd44307fad8
adamjstone <me@adamjstone.com>
2019-01-05 20:32:38 -0600

    Production deployment target TBD.
    (cherry picked from commit 6a5122c7688ebf58c41e4900790310bd1ca7229c)
    
    # Conflicts:
    #	cicd/Configuration.psm1

---
+++ cicd/Configuration.psm1
@@ -0,0 +1,33 @@
+# =================================================================================================================================
+# Copyright (c) RapidField LLC. Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+# =================================================================================================================================
+
+# This variable informs the package version for CI/CD builds. Release branches should be named using this value.
+$BuildVersion = "1.0.23-preview"
+
+$Configuration = [PSCustomObject]@{
+    Local = [PSCustomObject]@{
+        PackageDeploymentUri = "C:\LocalPackageSource"
+    }
+    Production = [PSCustomObject]@{
+        PackageDeploymentUri = "TBD"
+    }
+}
+
+# Get
+# =================================================================================================================================
+
+function GetBuildVersion {
+    return $BuildVersion
+}
+
+function GetConfigurationValue {
+    Param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [String] $Type,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [String] $Key
+    )
+
+    return $Configuration.$Type.$Key
+}
