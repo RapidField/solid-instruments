a187f24fc7af3e2c6a2ffc0948dc009128ab8087
Adam Stone <8525409+adamjstone@users.noreply.github.com>
2019-07-07 23:16:11 -0500

    Improved CI/CD scripts.

---
+++ Build.bat
@@ -0,0 +1,7 @@
+:: =================================================================================================================================
+:: Copyright (c) RapidField LLC. Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+:: =================================================================================================================================
+
+:: This script is intended to be used in containerized build environments. Successful execution requires administrative privilege on
+:: the host because automation tools are installed during the process.
+Powershell.exe cicd\ExecuteCicdBuild.ps1
--- cicd/ExecuteCicdBuild.ps1
+++ cicd/ExecuteCicdBuild.ps1
@@ -2,7 +2,14 @@
 # Copyright (c) RapidField LLC. Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 # =================================================================================================================================
 
+Write-Host -ForegroundColor Cyan $("`nStarting a CI/CD build at {0:yyyy-MM-dd} {0:HH:mm:ss}." -f (Get-Date))
+
+# Establish automation tools.
 cd cicd
 .\ResetEnvironment.ps1
+
+# Execute the build.
 cd ..
 psake ta
+
+Write-Host -ForegroundColor Cyan $("`nCompleting a CI/CD build at {0:yyyy-MM-dd} {0:HH:mm:ss}." -f (Get-Date))
