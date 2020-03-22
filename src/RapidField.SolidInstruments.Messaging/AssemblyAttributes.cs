96d1c5d17271ebf511efe7dbc063d8ae778e42f0
adamjstone <8525409+adamjstone@users.noreply.github.com>
2020-03-22 09:16:15 -0500

    Adding tests for in-memory messaging abstractions.

--- src/RapidField.SolidInstruments.Messaging/AssemblyAttributes.cs
+++ src/RapidField.SolidInstruments.Messaging/AssemblyAttributes.cs
@@ -7,5 +7,6 @@ using System.Runtime.CompilerServices;
 [assembly: DisablePrivateReflection()]
 [assembly: InternalsVisibleTo("RapidField.SolidInstruments.Messaging.AzureServiceBus")]
 [assembly: InternalsVisibleTo("RapidField.SolidInstruments.Messaging.InMemory")]
+[assembly: InternalsVisibleTo("RapidField.SolidInstruments.Messaging.InMemory.UnitTests")]
 [assembly: InternalsVisibleTo("RapidField.SolidInstruments.Messaging.RabbitMq")]
 [assembly: InternalsVisibleTo("RapidField.SolidInstruments.Messaging.UnitTests")]
---
+++ test/RapidField.SolidInstruments.Messaging.InMemory.UnitTests/InMemoryClientFactoryTests.cs
@@ -0,0 +1,34 @@
+﻿// =================================================================================================================================
+// Copyright (c) RapidField LLC. Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+// =================================================================================================================================
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RapidField.SolidInstruments.Messaging.TransportPrimitives;
+using RapidField.SolidInstruments.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RapidField.SolidInstruments.Messaging.InMemory.UnitTests
+{
+    [TestClass]
+    public class InMemoryClientFactoryTests
+    {
+        [TestMethod]
+        public void GetQueuePath_Should()
+        {
+            // Arrange.
+            var serializationFormat = SerializationFormat.CompressedJson;
+
+            using (var transport = new MessageTransport(serializationFormat))
+            {
+                using (var connection = transport.CreateConnection())
+                {
+                    using (var target = new InMemoryClientFactory(connection))
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
